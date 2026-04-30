c417e0b7e959f66257698924eee10dde3605f9fc
nicholas-S425 <nsschlamp@gmail.com>
2026-04-30 11:31:20 -0700

    added delete color and a little more

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,46 @@
-import { removeItem } from 'framer-motion';
 import { useState } from 'react';
 
-
 function App() {
- const [text, setText] = useState("");
- const [list, setList] = useState([]);
- 
- const handleAdd = () => {
-   setList([...list,text]);
- };
-   const handleDelete = () => {
-    setItems(items.filter(item => item.id !== id));
- };
-return (
- <>
-   <h1>list tihngy</h1>
-   <div style={{padding:'20px'}}>
-     <input
-       type="text"
-       onChange={(e) => setText(e.target.value)}
-       placeholder="Type something..."
-     />
-     <button onClick={handleAdd}>
-       Add
-     </button>
-       {list.map((item) => (
-       <>
-       <li>{item}</li>
-         <button onClick={handleDelete}>
-         Delete
-         </button>
-       </>
-       ))}
-   </div>
- </>
-);
+  const [text, setText] = useState("");
+  const [color, setColor] = useState("#fafafaff");
+  const [list, setList] = useState([]);
+  
+  const handleAdd = () => {
+    if (!text.trim()) {
+      return
+    }
+    setList([...list, text]);
+    setText("")
+  };
+  const handleDelete = (indexToRemove) => {
+    setList(list.filter((_, index) => index !== indexToRemove));
+  };
+  return (
+    <>
+      <h1>shopping list</h1>
+        <input type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="put item here"
+        />
+        <input
+          type="color" 
+          value={color} 
+          onChange={(e) => setColor(e.target.value)} 
+        />
+        <button onClick={handleAdd}>Add</button>
+        <ol>
+        {list.map((item, index) => (
+          <div key={index}>
+            <li>{item}</li>
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </div>
+        ))}
+        <div style={{ backgroundColor: color, height: '100vh'}}></div>
+        </ol>
+    </>
+  );
 }
 
 
 export default App;
-
